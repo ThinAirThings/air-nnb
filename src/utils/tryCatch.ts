b315bab23fb38e4625f6c3ba71e0f73f1fc52231
irishcarbomb777 <danielclannan@gmail.com>
2024-03-07 12:11:55 -0500

    first commit

---
+++ src/utils/tryCatch.ts
@@ -0,0 +1,11 @@
+
+
+
+export const tryCatch = <T, R>(callback: (input: T) => R) => {
+    try {
+        return callback;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
